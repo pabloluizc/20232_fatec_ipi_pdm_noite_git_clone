c38de862d03b1a5ae3a4377651f3bdf78b7b27a4
Adaylton Pacheco <pablo.silva23@fatec.sp.gov.br>
2023-08-18 21:58:07 -0300

    git-python(ex_introducao): funcao menu

--- menu.py
+++ menu.py
@@ -1,23 +1,26 @@
 import calculadora
 n1 = int(input('Primeiro valor: '))
 n2 = int(input('Segundo valor: '))
-opcao = 0
-while opcao != 5:
-    print('''[ 1 ] somar
+print('''[ 1 ] somar
           [ 2 ] subtrair
           [ 3 ] multiplicar
           [ 4 ] dividir
-          [ 5 ] sair do menu''')
-    opcao = int(input('>>> Qual a opcao desejada? '))
-    if opcao == 1:
-        print(calculadora.somar(n1, n2))
-    elif opcao == 2:
-        print(calculadora.subtrair(n1, n2))
-    elif opcao ==3:
-        print(calculadora.multiplicar(n1, n2))
-    elif opcao ==4:
-        print(calculadora.dividir(n1, n2))
-    else:
-        print('Opcao invalida. Tente novamente.')
+          [ 0 ] sair do menu''')
+opcao = int(input('>>> Qual a opcao desejada? '))
+
+def menu2(opcao):
+    while opcao != 0:
+        if opcao == 1:
+            print(calculadora.somar(n1, n2))
+        elif opcao == 2:
+            print(calculadora.subtrair(n1, n2))
+        elif opcao == 3:
+            print(calculadora.multiplicar(n1, n2))
+        elif opcao == 4:
+            print(calculadora.dividir(n1, n2))
+        else:
+            print('Opcao invalida. Tente novamente.')
     print('===' * 10)
 print('Fim.')
+
+menu2(n1, n2)
