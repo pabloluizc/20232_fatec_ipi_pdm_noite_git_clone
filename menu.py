9c40d6693a320da6f8ecfa616bc1a081e3b6c230
Adaylton Pacheco <pablo.silva23@fatec.sp.gov.br>
2023-08-18 21:40:46 -0300

    git-python(ex_introducao): opcao somar

---
+++ menu.py
@@ -0,0 +1,16 @@
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+opcao = 0
+while opcao != 5:
+    print('''[ 1 ] somar
+          [ 2 ] subtrair
+          [ 3 ] multiplicar
+          [ 4 ] dividir
+          [ 5 ] sair do menu''')
+    opcao = int(input('>>> Qual a opcao desejada? '))
+    if opcao == 1:
+        print(calculadora.somar(1, 2))
+    else:
+        print('Opcao invalida. Tente novamente.')
+    print('===' * 10)
+print('Fim.')
